98e4b1a2e465c2f3f2b3c2962aa940bc06829974
Jôbert Kennedy <jobert.kennedy@outlook.com.br>
2020-04-11 15:02:26 -0300

    stable

---
+++ .dist/main.js
@@ -0,0 +1,69 @@
+const html = document.querySelector('html')
+const checkbox = document.querySelector('input#check')
+const container = document.querySelector('div.container')
+const headLabel = document.querySelector('label#headTxt')
+
+let appTheme = localStorage.getItem('theme') || 'light'
+const darkTheme = {
+    bgColor : '#121011',
+    contColor: '#122011',
+    fontColor: 'white'
+}
+const lightTheme = {
+    bgColor: 'white',
+    contColor: 'lightsalmon',
+    fontColor: 'rgb(65, 58, 58)'
+}
+
+
+function render(){
+    let colors
+    if(appTheme == 'light'){
+        appTheme = 'dark'
+        headLabel.innerHTML = `${appTheme.toUpperCase().charAt(0)}${appTheme.slice(1)} Theme`
+        colors = darkTheme
+        saveTheme()
+    } else if (appTheme == 'dark'){
+        appTheme = 'light'
+        headLabel.innerHTML = `${appTheme.toUpperCase().charAt(0)}${appTheme.slice(1)} Theme`
+        colors = lightTheme
+        saveTheme()
+    }
+    const { bgColor, contColor, fontColor } = colors
+    html.style.background = bgColor
+    container.style.background = contColor
+    container.style.color = fontColor
+
+    console.log(appTheme)
+}
+
+function saveTheme(){
+    localStorage.setItem('theme', appTheme)
+}
+
+function generateLabel(){
+    if(appTheme == 'light'){
+        headLabel.innerHTML = `Light Theme`
+    } else if (appTheme == 'dark'){
+        headLabel.innerHTML = `Dark Theme`
+    }
+}
+
+window.onload = () => {
+    if(appTheme == 'light'){
+        generateLabel()
+        const { bgColor, contColor, fontColor } = lightTheme
+        html.style.background = bgColor
+        container.style.background = contColor
+        container.style.color = fontColor
+        checkbox.checked = false
+    } else if (appTheme == 'dark'){
+        generateLabel()
+        const { bgColor, contColor, fontColor } = darkTheme
+        html.style.background = bgColor
+        container.style.background = contColor
+        container.style.color = fontColor
+        checkbox.checked = true
+    }
+}
+checkbox.addEventListener('change', render)
